acfca42d069d6f3042819bfa355f1d29e13f1d78
idejie <i@idejie.com>
2020-09-03 13:58:19 +0800

    Update generate_tsv.py

--- tools/generate_tsv.py
+++ tools/generate_tsv.py
@@ -172,20 +172,14 @@ def generate_tsv(gpu_id, prototxt, weights, image_ids, outfile):
             _t = {'misc' : Timer()}
             count = 0
             for im_file,image_id in image_ids:
-                if int(image_id) == 20450:
-#                 if int(image_id) in missing:
+                if int(image_id) in missing:
                     _t['misc'].tic()
                     writer.writerow(get_detections_from_im(net, im_file, image_id))
                     _t['misc'].toc()
                     if ((count+1)% 100) == 0:
                         print 'GPU {:d}: {:d}/{:d} {:.4f}s (projected finish: {:.4f} hours)' \
-                              .format(gpu_id, count+1, len(missing), _t['misc'].average_time,
+                              .format(gpu_id, count, len(missing), _t['misc'].average_time,
                               _t['misc'].average_time*(len(missing)-count)/3600)
-                        if ((count+1)%5000)==0:
-                            drive.flush_and_unmount()
-                            drive.mount('/content/drive',force_remount=True)
-                            import time
-                            time.sleep(5)
                     count += 1
 
 
