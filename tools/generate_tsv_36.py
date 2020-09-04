584512d4608cc6487fe0b1f34a92759c9a574e69
idejie <i@idejie.com>
2020-09-05 07:21:51 +0800

    Update generate_tsv_36.py

--- tools/generate_tsv_36.py
+++ tools/generate_tsv_36.py
@@ -190,7 +190,7 @@ def generate_tsv(gpu_id, prototxt, weights, image_ids, outfile):
                         print 'GPU {:d}: {:d}/{:d} {:.4f}s (projected finish: {:.4f} hours)' \
                               .format(gpu_id, count, len(missing), _t['misc'].average_time,
                               _t['misc'].average_time*(len(missing)-count)/3600)
-                    if (count+1)>0 and ((count+1))%10==0:
+                    if (count+1)>len(wanted_ids)*0.5 and ((count+1))%10000==0:
                         print('a half finished ')
                         command = 'gsutil cp /content/coco_resnet101_faster_rcnn_genome_36.h5.0 gs://flickr30k'
                         os.system(command)
